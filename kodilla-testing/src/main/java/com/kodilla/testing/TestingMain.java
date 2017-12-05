feed12ac0084618a26a4760d9a839728ac8799bc
walulikt <walulik.t@wp.pl>
2017-12-05 23:12:55 +0100

    Calculator Test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,37 @@
 package com.kodilla.testing;
 
-public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. wprowadzenie do testowania oprogramowania");
+import com.kodilla.testing.User.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
+public class TestingMain {
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calc = new Calculator();
+
+        int addTestResult = calc.add(3,2);
+        int subtractTestResult = calc.subtract(7,4);
+
+        if (addTestResult==5) {
+            System.out.println("Test dodwania: OK");
+        } else {
+            System.out.println("Error! - dodawanie");
+        }
+        if (subtractTestResult==(3)) {
+            System.out.println("Test odejmowania: OK");
+        } else {
+            System.out.println("Error! - odejmowania");
+        }
     }
 
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/User/SimpleUser.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.User;
+
+public class SimpleUser {
+    private String username;
+
+    public SimpleUser(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
+
