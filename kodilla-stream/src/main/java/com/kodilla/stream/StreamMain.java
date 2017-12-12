d0273180310296d62e25638565f0f1f807ec0e97
walulikt <walulik.t@wp.pl>
2017-12-12 23:53:22 +0100

    Poem Decoration

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,32 +1,17 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.ExecuteSaySomething;
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.reference.FunctionalCalculator;
 
+
+import com.kodilla.stream.com.kodilla.stream.beautifier.PoemBeautifier;
 
 public class StreamMain {
     public static void main (String [] args){
-        ExpressionExecutor expressionExecutor= new ExpressionExecutor();
+        PoemBeautifier poemBeautifier=new PoemBeautifier();
 
-        /*ZAMIAST: public void executeExpression(double a, double b, MathExpression mathExpression) {
-                        double result = mathExpression.calculateExpression(a, b);
-                        System.out.println("Result equals: " + result);
-                        TO:*/
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-            //LUB ODWOŁANIE BEZPOŚRENIO DO KLASY I METODY:
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-    }
+        poemBeautifier.beautify1( "Alfabet",text -> "ABC"+text+"ABC");
+        poemBeautifier.beautify2( "litle text",text -> text.toUpperCase());
+        poemBeautifier.beautify3( "Good day",text -> text.substring(0,4));
+        poemBeautifier.beautify4( "Piasek do maty",text -> text.replace('a','e'));
     }
 }
+
