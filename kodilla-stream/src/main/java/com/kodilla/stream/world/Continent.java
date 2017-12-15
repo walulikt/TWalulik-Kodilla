066e1cecdb086f9a0e36d78a6394998d44125640
walulikt <walulik.t@wp.pl>
2017-12-15 20:14:00 +0100

    Strem modul

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Continent.java
@@ -0,0 +1,21 @@
+package com.kodilla.stream.world;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Continent {
+
+   private final List<Country> theContinent = new ArrayList<>();
+
+     public List<Country> addCountry(Country country) {
+            theContinent.add(country);
+            return theContinent;
+        }
+
+    public List<Country> addConuntry() {
+
+        return theContinent;
+    }
+
+
+}
