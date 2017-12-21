b68075d9f5b5b561dba215b384174be37b3a82f9
walulikt <walulik.t@wp.pl>
2017-12-21 21:50:12 +0100

    Exception second try

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile() {
+    public void readFile() throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names").getFile());
 
@@ -18,7 +18,7 @@ public class FileReader {
 
             fileLines.forEach(System.out::println);
         } catch (IOException e) {
-            System.out.println("Oh no!Something went wrong! Error: " + e);
+            throw new FileReaderException();
         } finally {
             System.out.println("I am gonna be here... always!");
         }
