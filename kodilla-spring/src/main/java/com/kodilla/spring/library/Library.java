1780b07f5b9584f683559e2c04262c84b2032c47
walulikt <walulik.t@wp.pl>
2018-01-07 23:40:01 +0100

    Reader i uzupełnienie Library

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -9,8 +9,13 @@ import java.util.List;
 @Service
 public final class Library {
     private final List<String> books = new ArrayList<>();
-    @Autowired
     private LibraryDbController libraryDbController;
+
+    public Library(final LibraryDbController libraryDbController) {
+        this.libraryDbController = libraryDbController;
+    }
+    public Library() {
+    }
 
     public void saveToDb() {
         libraryDbController.saveData();
