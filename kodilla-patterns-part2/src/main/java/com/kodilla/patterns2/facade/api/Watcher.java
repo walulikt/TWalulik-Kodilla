f7dbcac10f65cd54ba46df991a2bcad4db6a3539
walulikt <walulik.t@wp.pl>
2018-03-20 23:18:13 +0100

    Aspect 20.3

--- kodilla-patterns-part2/src/main/java/com/kodilla/patterns2/facade/api/Watcher.java
+++ kodilla-patterns-part2/src/main/java/com/kodilla/patterns2/facade/api/Watcher.java
@@ -1,4 +1,18 @@
 package com.kodilla.patterns2.facade.api;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class Watcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(com.kodilla.patterns2.dao.calculator.Watcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public void logEvent() {
+        LOGGER.info("ProcessOrder method has been lunched!");
+    }
 }
