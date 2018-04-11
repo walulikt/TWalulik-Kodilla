23ec477e796b0a73449f78684017ffe21a1cb4dd
walulikt <walulik.t@wp.pl>
2018-04-11 20:22:22 +0200

    Selenium WebDriver p2

--- kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -1,9 +1,20 @@
 package com.kodilla.testing2.ebay;
 
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class EBayTestingApp {
+    public static final String SEARCHELEMENT = "_nkw";
 
     public static void main(String[] args){
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+        driver.get("https://www.ebay.com");
 
+        WebElement searchElement = driver.findElement(By.name(SEARCHELEMENT));
+        searchElement.sendKeys("laptop");
+        searchElement.submit();
 
     }
 }
